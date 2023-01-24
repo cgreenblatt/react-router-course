47cf829b3250a35283b207ac8b8a0eb4fee9df7e
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-01-24 15:33:14 -0800

    fix: Update packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <div className="yellow-border" />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
