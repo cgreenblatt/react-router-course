7e9f17cad165c693cbd3690d572972a29911230e
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-01-25 14:22:06 -0800

    feat: Add Navbar component and routing functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Teams from './Teams';
+import Players from './Players'
+
 export default function App () {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
+    <Router>
+      <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Players" element={<Players />} />
+            <Route path="/Teams" element={<Teams />} />
+            </Routes>
+      </div>
+    </Router>
   )
 }
