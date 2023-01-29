16dcc49f8563cad73e601d2b7a30ceac3c1cae6f
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-01-29 14:51:43 -0800

    feat: Add Sidebar component to Players component

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,7 +1,54 @@
+import { Link, useSearchParams, useLocation } from 'react-router-dom';
+import usePlayerNames from '../hooks/usePlayerNames';
+import { slugify } from '../utils';
+
+function CustomLink({ to, children }) {
+  const { pathname, search } = useLocation();
+  const playerId = pathname.split('/')[2];
+  const match = playerId === to;
+
+  const styles = match ? { fontWeight: 900, color: 'var(--white)' } : {};
+
+  return (
+    <li>
+      <Link
+        style={styles}
+        to={{
+          pathname: to,
+          search,
+        }}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+function Sidebar({ title, list }) {
+  return (
+    <div>
+      <h3 className="header">{title}</h3>
+      <ul className="sidebar-list">
+        {list.map((item) => (
+          <CustomLink key={item} to={slugify(item)}>
+            {item.toUpperCase()}
+          </CustomLink>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function Players() {
-    return (
-        <div className='container'>
-            Players
-        </div>
-    )
+  const [sp] = useSearchParams();
+  const team = sp.get('teamId');
+  const { response: names, loading } = usePlayerNames(team);
+
+  if (loading) return null;
+
+  return (
+    <div className="container">
+      <Sidebar title="Players" list={names} />
+    </div>
+  );
 }
